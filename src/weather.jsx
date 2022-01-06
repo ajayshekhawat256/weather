93963925cb697f4959c17796ca0727f52ac6a771
Ajay <Ajayshekhawat256@gmail.com>
2022-01-06 16:42:23 +0530

    finalcommit

---
+++ src/weather.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+
+const weather= () => {
+    return (
+        <div>
+            <h1> Weather app </h1>
+            <input type="text" />
+        </div>
+    )
+}
+
+export default weather
